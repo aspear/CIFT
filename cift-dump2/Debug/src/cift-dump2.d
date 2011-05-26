19dabe2cbaae9ad1b964316ffe70edd629c27e8e
Aaron Spear <aaron@ontherock.com>
2011-05-26 16:32:29 -0600

    removed Debug dir

--- cift-dump2/Debug/src/cift-dump2.d
+++
@@ -1,9 +0,0 @@
-src/cift-dump2.d src/cift-dump2.o: ../src/cift-dump2.cpp \
- ../../cift/cift.h ../src/address-range-lookup.h \
- ../src/nm-symbol-extractor.h
-
-../../cift/cift.h:
-
-../src/address-range-lookup.h:
-
-../src/nm-symbol-extractor.h:
